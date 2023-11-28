5086591a3414aa3e219ac70a2cf1dafc97ce5089
pimpwhippa <utrechtmay@gmail.com>
2023-11-28 23:39:48 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use an official Python runtime as a parent image
+FROM python:3.8-slim
+
+# Set the working directory in the container
+WORKDIR /usr/src/app
+
+# Copy the current directory contents into the container at /usr/src/app
+COPY . .
+
+# Install any needed packages specified in requirements.txt --no-cache-dir
+RUN pip install --progress-bar off -r requirements.txt
+
+# Make port 80 available to the world outside this container
+EXPOSE 5000
+
+RUN adduser myuser
+USER myuser
+
+# Define environment variable
+ENV FLASK_APP=wsgi.py
+
+# Run app.py when the container launches
+CMD gunicorn --bind 0.0.0.0:$PORT wsgi:app
---
+++ audio_classifier_app/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+#from audio_classifier import AudioClassifier  
+# # Assuming you have the AudioClassifier module in a separate file
+
+app = Flask(__name__)
+
+# Set the path where uploaded audio files will be stored
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Allowed file extensions
+ALLOWED_EXTENSIONS = {'wav'}
+
+#def classify_genre(self,audio_file_path):
+        # Replace this with your actual model loading and classification logic
+        # This is a placeholder implementation
+        #return "jazz", 0.85
+
+# Function to check if a file has an allowed extension
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Route for the home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Route for handling file upload and classification
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = app.config['UPLOAD_FOLDER'] + '/' + filename
+        file.save(file_path)
+
+        # Make predictions using the AudioClassifier
+        #predicted_genre, confidence_score = audio_classifier.classify_genre(file_path)
+        predicted_genre, confidence_score = "jazz", 0.85
+
+        # Render the results
+        return render_template('result.html', filename=filename, predicted_genre=predicted_genre, confidence_score=confidence_score)
+
+    return redirect(request.url)
+
+if __name__ == '__main__':
+    app.run(debug=True)
